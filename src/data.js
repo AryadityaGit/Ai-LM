b104249b3029dad763ce64ebedcab53bec7f5829
Aryaditya Ashwin Hate <68547802+AryadityaGit@users.noreply.github.com>
2025-08-14 20:03:23 +0530

    Create src/data.js

---
+++ src/data.js
@@ -0,0 +1,11 @@
+export const conversationData = [
+  {
+    id: 1,
+    sender: 'user',
+    name: 'User',
+    message: "Hey again, Aryaditya! What's on your mind today?"
+  },
+  // ... other messages
+];
+
+export const summaryData = "The conversation is between a user and Aryaditya...";
